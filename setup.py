d2ce949a43ca9ad5687427fb8b9bd05c107bcee4
Mario Campos <mcampos@anl.gov>
2015-08-26 09:13:59 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lqmt',
+    version='1.0.0.b1',
+    description='Flexible framework that allows automation to process cyber threat information and update endpoint defense tools',
+    long_description='The Last Quarter Mile Toolset is flexible framework that allows automation to process cyber threat information (CTI) and update endpoint defense tools.',
+    url='https://github.com/anl-cyberscience/LQMToolset/',
+    author='The CFM Team',
+    author_email='fedhelp@anl.gov',
+    classifiers = [
+        # See: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+
+        # How mature is this project? Common values are
+        # Development Status :: 1 - Planning
+        # Development Status :: 2 - Pre-Alpha
+        # Development Status :: 3 - Alpha
+        # Development Status :: 4 - Beta
+        # Development Status :: 5 - Production/Stable
+        # Development Status :: 6 - Mature
+        # Development Status :: 7 - Inactive
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Information Technology',
+        'Topic :: Security',
+
+        # Pick your license as you wish (should match 'license' above)
+        'License :: Other/Proprietary License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='firewall endpoint last quarter mile toolset cisco palo alto',
+    packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
+    install_requires=['toml'],
+    scripts=['LQMTool.py'],
+    package_data={
+        'doc': ['LQMToolset_Getting_Started.pdf'],
+    },
+)
