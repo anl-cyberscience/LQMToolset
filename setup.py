51043d59749b5537a375d8b0702ef8118f9b201f
Mario Campos <mcampos@anl.gov>
2015-09-09 15:17:46 -0500

    bump to v1.0.0 for PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lqmt',
-    version='1.0.0.b1',
+    version='1.0.0',
     description='Flexible framework that allows automation to process cyber threat information and update endpoint defense tools',
     long_description='The Last Quarter Mile Toolset is flexible framework that allows automation to process cyber threat information (CTI) and update endpoint defense tools.',
     url='https://github.com/anl-cyberscience/LQMToolset/',
