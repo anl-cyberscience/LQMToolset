18c066d9eac8d4eeba9fa01bee4040d912d477f2
S. Sullivan <smsullivan@anl.gov>
2017-03-23 13:43:50 -0500

    Message formatting complete, sourcetype extraction, and updated key hashing
    - Message formatting is basically complete
    - Sourcetype is being extracted and removed from the message so that it
    can be used via the API
    - Key hashing of alert data, which is used to weed out duplicate alerts,
    is using a sha256 hash now
    - Remove redundant field remapping function. Part of this entails adding
    a list storing the valid sourcetype's. Before a message is sent, the
    sourcetype is checked against the list to ensure correct sourcetype
    usage.

--- lqmt/tools/to_mbl/tool.py
+++ lqmt/tools/to_mbl/tool.py
@@ -3,19 +3,7 @@ from lqmt.lqm.tool import Tool
 from lqmt.lqm.data import AlertAction
 from lqmt.tools.to_splunk.splunk_api import create_message, ApiHandler
 from lqmt.lqm.parsers.FlexTransform.parser import FlexTransformParser
-
-
-def field_remapping(alert):
-    """
-    Function used to translate field mappings from the intermediate data format to the the mbl supported format
-    :param alert:
-    :return:
-    """
-    mbl_lexicon = {
-        'blocking': {},
-        'spearphish': {},
-        'malware': {}
-    }
+import hashlib
 
 
 class ToMBL(Tool):
@@ -27,20 +15,23 @@ class ToMBL(Tool):
         super().__init__(config, [AlertAction.get('All')])
         self._logger = logging.getLogger("LQMT.ToolName.{0}".format(self.getName()))
         self.alerts = {}
+        self.sourcetype_lexicon = ['block', 'spearphish', 'malware']
 
         self._parser = FlexTransformParser({'mbl': 'resources/sampleConfigurations/MBL.cfg'})
         for file_type, source_config in self._config.source_configs.items():
             self._parser.add_parser(file_type, source_config)
 
-        # commenting out splunk handler until data is formmated correctly first
-        # self.handler = ApiHandler(
-        #     self._config.host,
-        #     self._config.port,
-        #     self._config.username,
-        #     self._config.password,
-        #     cert_check=self._config.cert_check,
-        #     source=self._config.source,
-        # )
+            # commenting out splunk handler until data is formmated correctly first
+            # self.handler = ApiHandler(
+            #     self._config.host,
+            #     self._config.port,
+            #     self._config.username,
+            #     self._config.password,
+            #     cert_check=self._config.cert_check,
+            #     source=self._config.source,
+            #     index=self._config.index,
+            #
+            # )
 
     def initialize(self):
         super().initialize()
@@ -52,32 +43,55 @@ class ToMBL(Tool):
         # parse alert using custom parser
         mbl_alert = self._parser.custom_parser(alert, meta['PayloadFormat'], 'mbl')
 
-        # pop alert data from list
-        mbl_alert = mbl_alert.pop()
+        if mbl_alert:
+            # pop alert data from list
+            mbl_alert = mbl_alert.pop()
 
-        # put alert data into a dictionary. key is generated from a hash of the list
-        self.alerts[hash(str(mbl_alert))] = mbl_alert
+            # put alert data into a dictionary. key is generated from a hash of the list
+            key_hash = self.compute_hash(str(mbl_alert))
+            if key_hash not in self.alerts:
+                self.alerts[key_hash] = mbl_alert
 
     def commit(self):
         print("committing")
-        print(self.parse_alert_dictionary())
 
-        # eventually, this is where the message will be sent from
-        # self.handler.send_message(create_message(alert), sourcetype="PULL FROM ALERT")
+        for dict_key, alert_data in self.alerts.items():
+            sourcetype, message = self.parse_alert_dictionary(alert_data)
+
+            print("Sourcetype: {0}; Message: {1};".format(sourcetype, message))
+
+            # eventually, this is where the message will be sent from
+            if sourcetype in self.sourcetype_lexicon:
+                pass
+                # self.handler.send_message(message), sourcetype=sourcetype)
 
     def cleanup(self):
         pass
 
-    def parse_alert_dictionary(self):
+    @staticmethod
+    def parse_alert_dictionary(alert_data):
         """
-        :return: returns a list of splunk/mbl formatted string generated from the alert dictionary
+        Method used for extracting the source type and message data from the provided alert data dictionary
+        :param alert_data: dictionary containing parsed alert data
+        :return: returns the sourcetype(str) and formatted message(str) both of which are derived from the alert_data
+        dictionary
         """
-        messages = []
-        for dict_key, alert_data in self.alerts.items():
-            alert_list = []
-            for alert_dictionary in alert_data:
-                for key, data in alert_dictionary.items():
-                    alert_list.append(str(key)+": "+str(data))
-            messages.append(' '.join(alert_list))
+        message = ""
+        sourcetype = ""
+        for alert_dictionary in alert_data:
+            if 'sourcetype' in alert_dictionary:
+                sourcetype = alert_dictionary['sourcetype']
+                del alert_dictionary['sourcetype']
+            for key, data in alert_dictionary.items():
+                message += str(key) + ": " + str(data) + " "
 
-        return messages
+        return sourcetype, message
+
+    @staticmethod
+    def compute_hash(string):
+        """
+        Method used for turning an alert string into a hash. Currently being used to sort out duplicate data.
+        :param string: Alert data in of the type Str
+        :return: returns sha256 digest of given string
+        """
+        return hashlib.sha256(string.encode('utf-8')).hexdigest()
