26a8b4be5d3e156cf265d7c9f81a1aeaa9449ddd
Sean Sullivan <smsullivan@anl.gov>
2018-09-05 13:10:46 -0500

    Updating Version
    Jumping from 1.4.1 to 1.4.2 because of an error of uploading an early version to python without including changes.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='lqmt',
-    version='1.4.1',
+    version='1.4.3',
     description='Flexible framework that allows automation to process cyber threat information and update endpoint defense tools',
     long_description='The Last Quarter Mile Toolset is flexible framework that allows automation to process cyber threat information (CTI) and update endpoint defense tools.',
     url='https://github.com/anl-cyberscience/LQMToolset/',
